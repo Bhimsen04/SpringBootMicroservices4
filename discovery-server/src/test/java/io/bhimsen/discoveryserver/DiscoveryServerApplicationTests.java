32d02b5fe14f2bb879f220d90d1d7687e38d0f4e
bhims <bhims@DESKTOP-5HI3COU>
2021-06-06 14:31:47 +0530

    First Commit Level1 Completes

---
+++ discovery-server/src/test/java/io/bhimsen/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bhimsen.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
